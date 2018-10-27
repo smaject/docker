b92eb0fc4bf8f4a69b831850b32759d505e4d079
Marc Pestel <mp@smaject.de>
2018-10-27 15:01:53 +0200

    Upgrade to Wildfly 14, which enables JEE8 per default

--- java/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Create OpenJdk 8 docker image"
-
-docker build -t smaject/java:8 .
---
+++ wildfly/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Create Wildfly base docker image"
+
+docker build -t smaject/wildfly .
